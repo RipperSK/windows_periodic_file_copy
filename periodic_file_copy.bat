739b9ca4d269341ae7f76a2aac34f7c3ee59e25d
RipperSK <roman.spiak@gmail.com>
2022-12-18 19:50:17 +0100

    Initial commit

---
+++ periodic_file_copy.bat
@@ -0,0 +1,41 @@
+@echo off
+
+set /p "srcpath=Enter path to save every X minutes: "
+set /p "ext=Enter file extension: "
+
+:: uncomment for testing
+::SET srcpath=C:\tmp
+::SET ext=txt
+::echo %srcpath%
+
+:infiloop
+
+cd %srcpath%
+
+::Get DATE
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+:: uncomment for testing
+::echo datestamp: "%datestamp%"
+::echo timestamp: "%timestamp%"
+::echo fullstamp: "%fullstamp%"
+
+:: loop to announce following actions
+for /f %%f in ('dir /b *.%ext%') do echo will copy %%f to %%f-%fullstamp%.bkp
+
+:: loop copy all files with given extension
+for /f %%f in ('dir /b *.%ext%') do copy %%f %%f-%fullstamp%.bkp
+
+:: Information of completed loop iteration
+echo Creation of %fullstamp% backups is done
+
+:: timeout time is in seconds
+echo Will sleep 10 minutes
+timeout /t 600 > nul
+
+:: horrible way to loop with labels :)
+goto infiloop
